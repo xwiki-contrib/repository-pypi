adb68be980408089bd5acfca7ba62b138b9459e6
krisss2121 <krzysiekplachno@gmail.com>
2017-07-18 11:30:23 +0200

    Accessing rest api to get package info

--- src/main/java/org/xwiki/contrib/repository/pypi/PypiExtensionRepository.java
+++ src/main/java/org/xwiki/contrib/repository/pypi/PypiExtensionRepository.java
@@ -19,7 +19,17 @@
  */
 package org.xwiki.contrib.repository.pypi;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.slf4j.Logger;
+import org.xwiki.contrib.repository.pypi.dto.PypiPackageJSONDto;
 import org.xwiki.extension.Extension;
 import org.xwiki.extension.ExtensionDependency;
 import org.xwiki.extension.ExtensionId;
@@ -32,22 +42,27 @@ import org.xwiki.extension.repository.http.internal.HttpClientFactory;
 import org.xwiki.extension.repository.result.IterableResult;
 import org.xwiki.extension.version.Version;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * @version $Id: 81a55f3a16b33bcf2696d0cac493b25c946b6ee4 $
  * @since 1.0
  */
 public class PypiExtensionRepository extends AbstractExtensionRepository
 {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
     private final ExtensionLicenseManager licenseManager;
 
     private final ExtensionFactory extensionFactory;
 
     private final HttpClientFactory httpClientFactory;
 
+    private final HttpClientContext localContext;
+
     private final Logger logger;
 
     /**
-     *
      * @param extensionRepositoryDescriptor -
      * @param licenseManager -
      * @param extensionFactory -
@@ -63,6 +78,7 @@ public class PypiExtensionRepository extends AbstractExtensionRepository
         this.licenseManager = licenseManager;
         this.extensionFactory = extensionFactory;
         this.httpClientFactory = httpClientFactory;
+        this.localContext = HttpClientContext.create();
         this.logger = logger;
     }
 
@@ -79,5 +95,42 @@ public class PypiExtensionRepository extends AbstractExtensionRepository
     @Override public IterableResult<Version> resolveVersions(String s, int i, int i1) throws ResolveException
     {
         return null;
+    }
+
+    protected Optional<PypiPackageJSONDto> getPypiPackageData(String packageName, String version)
+            throws HttpException, ResolveException
+    {
+        HttpGet getMethod = new HttpGet(
+                PypiParameters.PACKAGE_VERSION_INFO_JSON.replace("{package_name}",
+                        packageName).replace("{version}", version)
+        );
+        CloseableHttpClient httpClient = httpClientFactory.createClient(null, null);
+        CloseableHttpResponse response;
+        try {
+            if (this.localContext != null) {
+                response = httpClient.execute(getMethod, this.localContext);
+            } else {
+                response = httpClient.execute(getMethod);
+            }
+        } catch (Exception e) {
+            throw new HttpException(String.format("Failed to request [%s]", getMethod.getURI()), e);
+        }
+
+        int statusCode = response.getStatusLine().getStatusCode();
+        if (statusCode == HttpStatus.SC_OK) {
+            try {
+                PypiPackageJSONDto pypiPackageJSONDto =
+                        objectMapper.readValue(response.getEntity().getContent(), PypiPackageJSONDto.class);
+                return Optional.of(pypiPackageJSONDto);
+            } catch (IOException e) {
+                throw new HttpException(String.format("Failed to parse response body of request [%s]",
+                        getMethod.getURI()), e);
+            }
+        } else if (statusCode == HttpStatus.SC_NOT_FOUND) {
+            return Optional.empty();
+        } else {
+            throw new ResolveException(String.format("Invalid answer [%s] from the server when requesting [%s]",
+                    response.getStatusLine().getStatusCode(), getMethod.getURI()));
+        }
     }
 }
